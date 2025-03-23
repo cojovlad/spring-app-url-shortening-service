9ed4c43f242b104e773fb7e1c1cc48f3019e9758
Vlad-Andrei Cojocariu <vlad-andrei.cojocariu@ibm.com>
2025-03-23 17:03:16 +0200

    HWVE-0000 added all needed application properties, created epic for the app at index.html

---
+++ src/main/java/com/example/spring_app_url_shortening_service/SpringAppUrlShorteningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_app_url_shortening_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppUrlShorteningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppUrlShorteningServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_app_url_shortening_service/SpringAppUrlShorteningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_app_url_shortening_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppUrlShorteningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
